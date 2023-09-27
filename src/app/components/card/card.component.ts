f5bfc7b6b9c60f2080775df9c1a485401cbabdab
Guilherme <guilhermeemeterioif@gmail.com>
2023-09-27 15:33:59 -0300

    Base layout complete

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  @Input()
+  gameCover:string = '';
+  @Input()
+  gameLabel:string='Undefined';
+  @Input()
+  gameType:string = 'Undefined'
+  @Input()
+  gamePrice:string = 'Undefined'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
